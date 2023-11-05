f80c38dd3b7377beffda07afbd0a2d8660af01a4
VladimirSt13 <Vladimir.Stognij@gmail.com>
2023-11-05 14:26:48 +0200

    finish hw-03

--- js/filter.js
+++ js/filter.js
@@ -6,12 +6,18 @@
   buttons.forEach((input) => {
     input.addEventListener("change", (event) => {
       const cardType = event.target.value;
+
       buttons.forEach((el) => {
         el.parentElement.classList.toggle("selected", el === event.target);
       });
 
       cards.forEach((card) => {
-        card.classList.toggle("hidden", card.dataset.type !== cardType);
+        if (cardType === "all") {
+          card.classList.contains("visually-hidden") && card.classList.remove("visually-hidden");
+          return;
+        }
+
+        card.classList.toggle("visually-hidden", card.dataset.type !== cardType);
       });
     });
   });
